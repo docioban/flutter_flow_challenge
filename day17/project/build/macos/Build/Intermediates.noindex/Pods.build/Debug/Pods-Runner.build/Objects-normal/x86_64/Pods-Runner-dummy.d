ef7c78d393da1636270af6b7158aaa4fa8154bf3
Dorin Ciobanu <dorinciobanu17@gmail.com>
2023-01-10 23:09:44 +0200

    day 17

---
+++ day17/project/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods-Runner-dummy.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/macbookpro/Desktop/flutter_forward/day17/project/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
+  /Users/macbookpro/Desktop/flutter_forward/day17/project/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap
---
+++ day17/project/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_macos.build/Objects-normal/x86_64/path_provider_macos-dummy.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/macbookpro/Desktop/flutter_forward/day17/project/macos/Pods/Target\ Support\ Files/path_provider_macos/path_provider_macos-dummy.m \
+  /Users/macbookpro/Desktop/flutter_forward/day17/project/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_macos.build/module.modulemap \
+  /Users/macbookpro/Desktop/flutter_forward/day17/project/macos/Pods/Target\ Support\ Files/path_provider_macos/path_provider_macos-prefix.pch
